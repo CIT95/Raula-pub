ae5d400bcd3bc430e63c860c2e25fd6dc1df91e3
Raul <raulaguilar360@gmail.com>
2022-01-20 18:59:52 -0800

    added more line breaks

--- w2/main.py
+++ w2/main.py
@@ -8,10 +8,10 @@ weight = float(input("enter your weight in kg: "))
 BMI = int(weight / (height * height))
 
 print(BMI + """Hello the following is on to interpret the results above. \n
-if you are below 18.5 you are considered underweight
-between 18.5 - 24.9 Healthy Weight
-between 25.0 - 29.9 Overwieght
-and 30 and above is Obsity
+if you are below 18.5 you are considered underweight \n
+between 18.5 - 24.9 Healthy Weight \n
+between 25.0 - 29.9 Overwieght \n 
+and 30 and above is Obsity \n 
 This is all based on you being 20 years old or older""")
 
 
