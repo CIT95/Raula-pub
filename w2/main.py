3a75041d1e570513bfbd9172afaa287bad2a5070
Raul <raulaguilar360@gmail.com>
2022-01-20 19:23:22 -0800

    pep8

--- w2/main.py
+++ w2/main.py
@@ -10,8 +10,6 @@ BMI = int(weight / (height * height))
 print(str(BMI) + "\n" + """Hello the following is on to interpret the results above. \n
 if you are below 18.5 you are considered underweight \n
 between 18.5 - 24.9 Healthy Weight \n
-between 25.0 - 29.9 Overwieght \n 
-and 30 and above is Obsity \n 
+between 25.0 - 29.9 Overwieght \n
+and 30 and above is Obsity \n
 This is all based on you being 20 years old or older""")
-
-
